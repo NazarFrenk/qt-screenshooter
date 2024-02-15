a38acdf964ee98316178a21f146fadd0a7809476
nazarfrenk77 <nazarfrenk77@gmail.com>
2024-02-15 23:08:49 +0200

    Add button

--- QtScreenshoter_uk_UA.ts
+++ QtScreenshoter_uk_UA.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk_UA"></TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <source>QtScreenshoter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="27"/>
+        <source>Start/Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
